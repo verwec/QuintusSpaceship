405c6e92d4285b714c7e90c14d3cdbcadadeacff
verwec <sebastian@vertragswecker.de>
2016-03-22 08:16:19 +0100

    Draw the player
    * add player.png
    * add Q.Player class

--- Game.js
+++ Game.js
@@ -2,10 +2,24 @@ var Q = Quintus()
   .include('Sprites')
   .setup({ width: 800, height: 480 });
 
-  Q.load(['back.png'], function(){
+  Q.Sprite.extend('Player', {
+    init: function(p){
+      this._super(p, {
+        asset: 'player.png',
+        x: Q.el.width/2,
+        y: Q.el.height-60,
+        type: Q.SPRITE_FRIENDLY,
+        speed: 10
+      })
+    }
+  });
+
+  Q.load(['back.png', 'player.png'], function(){
     var background = new Q.Sprite({ asset: 'back.png', x: Q.el.width/2, y: Q.el.height/2, type: Q.SPRITE_NONE });
+    var player = new Q.Player();
     Q.gameLoop(function(dt){
       Q.clear();
       background.render(Q.ctx);
+      player.render(Q.ctx);
     });
   });
