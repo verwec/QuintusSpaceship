8c6af08753933a18021606737bffeaad683124d9
verwec <sebastian@vertragswecker.de>
2016-03-22 08:56:22 +0100

    Animate spaceship
    * add player_anim.png
    * add player.json
    * enable player animation

--- Game.js
+++ Game.js
@@ -1,25 +1,31 @@
 var Q = Quintus()
-  .include('Sprites')
+  .include('Sprites, Anim')
   .setup({ width: 800, height: 480 });
 
   Q.Sprite.extend('Player', {
     init: function(p){
       this._super(p, {
-        asset: 'player.png',
+        sprite: 'player',
+        sheet: 'player',
         x: Q.el.width/2,
         y: Q.el.height-60,
         type: Q.SPRITE_FRIENDLY,
         speed: 10
-      })
+      });
+      this.add("animation");
+      this.play("default");
     }
   });
 
-  Q.load(['back.png', 'player.png'], function(){
+  Q.load(['back.png', 'player.png', 'player_anim.png', 'player.json'], function(){
+    Q.compileSheets("player_anim.png", "player.json")
+    Q.animations("player", { default: { frames: [0,1,2,3], rate: 1/4 }});
     var background = new Q.Sprite({ asset: 'back.png', x: Q.el.width/2, y: Q.el.height/2, type: Q.SPRITE_NONE });
     var player = new Q.Player();
     Q.gameLoop(function(dt){
       Q.clear();
       background.render(Q.ctx);
+      player.update(dt);
       player.render(Q.ctx);
     });
   });
