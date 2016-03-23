b4143a3ef15f22c8c5f9fe9ef24b4ffa59586b1d
verwec <sebastian@vertragswecker.de>
2016-03-23 06:34:56 +0100

    Convert to stages
    * add scene mainLevel
    * move Sprite & Player inserts to scene

--- Game.js
+++ Game.js
@@ -4,9 +4,10 @@ var clamp = function(x, min, max){
 }
 
 var Q = Quintus()
-  .include('Sprites, Anim, Input, Touch')
+  .include('Sprites, Anim, Input, Touch, Scenes')
   .setup({ width: 800, height: 480 })
-  .controls();
+  .controls()
+  .touch();
 
   Q.Sprite.extend('Player', {
     init: function(p){
@@ -35,15 +36,14 @@ var Q = Quintus()
     }
   });
 
+  Q.scene("mainLevel", function(stage){
+    Q.gravity=0;
+    stage.insert(new Q.Sprite({ asset: 'back.png', x: Q.el.width/2, y: Q.el.height/2, type: Q.SPRITE_NONE }))
+    stage.insert(new Q.Player());
+  });
+
   Q.load(['back.png', 'player.png', 'player_anim.png', 'player.json'], function(){
     Q.compileSheets("player_anim.png", "player.json")
     Q.animations("player", { default: { frames: [0,1,2,3], rate: 1/4 }});
-    var background = new Q.Sprite({ asset: 'back.png', x: Q.el.width/2, y: Q.el.height/2, type: Q.SPRITE_NONE });
-    var player = new Q.Player();
-    Q.gameLoop(function(dt){
-      Q.clear();
-      background.render(Q.ctx);
-      player.update(dt);
-      player.render(Q.ctx);
-    });
+    Q.stageScene("mainLevel");
   });
